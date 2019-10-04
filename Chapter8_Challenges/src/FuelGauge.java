6b605b369cfce92ac7c64d985becda947278df7b
student <student@10.50.2.79>
2019-10-04 10:53:47 +0700

    Chapter8_Challenges

--- Chapter8_Challenges/src/FuelGauge.java
+++ Chapter8_Challenges/src/FuelGauge.java
@@ -1,4 +1,33 @@
 
 public class FuelGauge {
+	private int currentAmount;
+	private int increaseFuel;
+	private int decreaseFuel;
+	public FuelGauge(int currentAmount, int increaseFuel, int decreaseFuel) {
+		super();
+		this.currentAmount = currentAmount;
+		this.increaseFuel = increaseFuel;
+		this.decreaseFuel = decreaseFuel;
+	}
+	public int getCurrentAmount() {
+		return currentAmount;
+	}
+	public void setCurrentAmount(int currentAmount) {
+		this.currentAmount = currentAmount;
+	}
+	public int getIncreaseFuel() {
+		return increaseFuel;
+	}
+	public void setIncreaseFuel(int increaseFuel) {
+		this.increaseFuel = increaseFuel;
+		increaseFuel = 0;
+		//for(in)
+	}
+	public int getDecreaseFuel() {
+		return decreaseFuel;
+	}
+	public void setDecreaseFuel(int decreaseFuel) {
+		this.decreaseFuel = decreaseFuel;
+	}
 
 }
--- Chapter8_Challenges/src/Odometer.java
+++ Chapter8_Challenges/src/Odometer.java
@@ -1,4 +1,24 @@
 
 public class Odometer {
-
+	private double currentMile;
+	private double odometer = 999.999;
+	private FuelGauge fuelgauge;
+	public double getCurrentMile() {
+		return currentMile;
+	}
+	public void setCurrentMile(double currentMile) {
+		this.currentMile = currentMile;
+	}
+	public double getOdometer() {
+		return odometer;
+	}
+	public void setOdometer(double odometer) {
+		this.odometer = odometer;
+	}
+	public FuelGauge getFuelgauge() {
+		return fuelgauge;
+	}
+	public void setFuelgauge(FuelGauge fuelgauge) {
+		this.fuelgauge = fuelgauge;
+	}
 }
